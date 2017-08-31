3d7c7373b7262eb85ecf5aba3d7e10ec0e8b3b49
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-08-31 12:05:46 +0530

    Create ye.c

---
+++ ye.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter the year");
+scanf("%d",&i);
+if(i%4==0)
+{
+print("leap year");
+}
+else
+{
+printf("not leap year");
+}
+}

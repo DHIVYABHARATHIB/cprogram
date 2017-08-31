bed8f84c6a48aac1fd3801fd8fda4cea1a9a401a
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-08-31 10:59:27 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+pritnf("enter the number");
+scamf("%d",&a);
+if(a>0)
+{
+printf("no is positive");
+}
+else
+{
+printf(no is negative");
+}
+}
+

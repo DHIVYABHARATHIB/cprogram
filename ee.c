26c8842f52f0ae26c4b5e92428ad418b1dc642db
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-05 19:42:51 +0530

    Create ee.c

---
+++ ee.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char h;
+printf("enter the character");
+scanf("%s",&h);
+if((h>=a && h<=z)||(h>>=A && h<=Z))
+{
+printf("character is alphabet");
+}
+else
+{
+printf("not a alphabet");
+}
+}
+

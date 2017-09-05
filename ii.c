7b00c0927171a0eab025c89aeab920f4f7e7ac25
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-05 19:33:26 +0530

    Create ii.c

---
+++ ii.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[5]={'a','e','i','o','u'};
+char g;
+printf("enter the character");
+scanf("%s",&g);
+if(a[5]==g)
+{
+printf("character is vowel");
+}
+else
+{
+printf("character is consonanat");
+}
+}
+
+

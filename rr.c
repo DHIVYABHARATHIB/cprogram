d1688d9a12acb3d307f32968944d7e2553eedee6
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-05 19:19:41 +0530

    Create rr.c

---
+++ rr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the three values");
+if(a>>b&&a>>c)
+{
+printf("a is greater");
+elseif(b>>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}

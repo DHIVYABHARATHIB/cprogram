38d5eb75b207cb8044e079133f47bc7a217c2b34
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-06 19:03:44 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,b,e;
+print("enter the value");
+scanf("%d",&b);
+printf("enter the value");
+scanf("%d",&e);
+r=pow(b,e);
+printf("enter the result");
+}

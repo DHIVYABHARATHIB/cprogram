d5fc3341448873bdd5f82a5dcef592355cb517f6
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-06 19:19:28 +0530

    Create ww.c

---
+++ ww.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,m,flag=0;
+printf("emter the number");
+scanf("%d",&l);
+for(i=2;i=n/2;++i)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+if(flag==0)
+{
+printf("number is prime");
+}
+else
+{
+printf("number is not prime");
+}
+}
+}
+
+
+

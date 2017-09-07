89035d1963ae3be52f4e342fc716ed6dd9f743ef
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-07 18:50:25 +0530

    Create yy.c

---
+++ yy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no,l=20,r=40;
+printf("enter the odd number between two value");
+for(no=l;no<=r;no++)
+{
+if(no%2==1)
+{
+printf("num is");
+}
+}
+}

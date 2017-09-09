f4e75279cad04f7ef4dac1765a1e1c177975dbc8
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-09 19:17:12 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n fact=0;
+printf("enter the number");
+scanf("%d",&n);
+if(n<0)
+{
+printf("factorial not exist");
+}
+else
+{
+for(i=1;i<=n;i++)
+{
+factorial*=i;
+}
+printf("enter the factorial");
+}
+}

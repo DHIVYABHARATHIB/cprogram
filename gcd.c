a3bcc8a00bed4a6c306666d89a2588c65d17ab16
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-10 15:54:33 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+vod main()
+{
+int i,t1,12,gcd;
+printf("enter the two values");
+scanf("%d%d",&t1.&t2);
+for(i=1;i>=t1&&i<=t2;++i)
+{
+if(t1%2==0&&t2%2==0)
+{
+gcd=i;
+printf("t1,t2,gcd");
+}
+}
+}

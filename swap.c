505d8de818961dfff757ecdb73cdf1fde42ce587
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-24 18:16:16 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t1,t2,n;
+printf("enter the two number");
+scanf("%d%d",&t1,&t2);
+n=t1;
+t1=t2;
+t2=n;
+printf("enter the value after swapping");
+}

37f92bbb9217776f19277e0f42a7af03790fea81
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-24 17:07:46 +0530

    Create three.c

---
+++ three.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t1,t2,t3;
+printf("enter the three number");
+scanf("%d%d%d",&t1&t2&t3);
+if(t1>=t2&&t1>t3)
+{
+printf("%d",t1);
+}
+if(t2>t1&&t2>>t3)
+{
+printf("%",t2);
+}
+if(t3>t1&&t3>>t2)
+{
+printf("%d",t3);
+}
+}

4e387e4df10fcee8cb892fe445789384f1f5fcb2
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-25 19:13:24 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#includeconio.h>
+viod main()
+{
+int n t=0,r;
+printf("enter th number");
+scanf("%d",&n);
+while(n!=0)
+{
+r=n%10;
+t=t*10+r;
+n/=10
+}
+printf("enter the reversed number");
+}

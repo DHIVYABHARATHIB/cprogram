f67bf6b03a395d5fbb76e909e556a830670d0561
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-26 19:05:57 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t1,t2,n;
+printf("enter the two number");
+scanf("%d",&t1,&t2);
+n={t1>t2) ? t1:t2
+while(1)
+{
+if(n%t1==0&&n%t2==0)
+{
+printf("the value of t1,t2,n");
+++n;
+}
+}
+}

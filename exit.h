17a3ccbe6651b20b0097baf846e2f20f7a0b4eb6
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-26 19:22:10 +0530

    Create exit.h

---
+++ exit.h
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int char c;
+int i
+printf("enter the 1 to 5 again and again");
+while(1)
+{
+for(i=0;i<=5;i++)
+{
+printf("%d",i);
+ch=getch();
+if(ch=Q)
+exit(0);
+}
+}
+}

cf33cdb0f4b9f0e7162c650d7b63b5c05760179b
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-27 19:23:38 +0530

    Create low.c

---
+++ low.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>a[i]
+void main()
+{
+int a[5],h,l,i;
+for(i=0;i<=5;i++)
+{
+printf("enter the value %d",i+1);
+scanf("%d",&a[5]);
+}
+h=a[0];
+l=a[0];
+for(i=0;i<=5;i++)
+{
+if(a[5]>=h)
+{
+h=a[i];
+else
+{
+if(a[5]<=l)
+{
+l=a[i];
+}
+}
+printf("enter the value if l");
+printf("enter the value of h");
+}
+

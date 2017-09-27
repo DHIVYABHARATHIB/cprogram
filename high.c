7cea5a1f6dc54a42136ae9689c6fb8b807e14bf6
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-09-27 19:41:05 +0530

    Create high.c

---
+++ high.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10]={1,2,3,4,5,6,7,8,9,0};
+int sum,loop;
+float avg;
+sum=avg=0;
+for(loop=0;loop<=10;loop++)
+{
+sum=sum+a[10];
+}
+avg=float(sum)/loop;
+printf(enter the value of avg");
+}

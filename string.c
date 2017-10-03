3d8982932224d5bdce56f28cc0bb172657586b30
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-10-03 19:21:51 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str1[50],str2[50],i,j;
+printf("enter the first string");
+scanf("%c",&str1);
+printf("enter the second string");
+scanf("%c",&str2);
+for(j=0;str2[j]!=0;++j ++i)
+{
+str[i]=str[j];
+}
+str1[i]='\0';
+printf("%c",str1);
+}

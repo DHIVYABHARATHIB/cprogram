1d4ef9eb74c00eda14daa0cce57f376ed13d93a7
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2017-10-04 19:35:00 +0530

    Create tear.c

---
+++ tear.c
@@ -0,0 +1,20 @@
+#inlude<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[50],rev[50];
+int i,j,k;
+printf("enter the string");
+scanf("%d",&srt[50]);
+printf("%d",str[50);
+for(i=0;str[i]='/0\;i++)
+{
+k=i-1;
+}
+for(j=0;j<=i-1;j++)
+{
+rev[j]=str[k];
+k--;
+}
+printf("%d",str[50]);
+}

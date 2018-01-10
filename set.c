785d62c9d8c5a5e386e34a9d604242c3b4556aa9
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-10 19:16:39 +0530

    Create set.c

---
+++ set.c
@@ -0,0 +1,18 @@
+#include<stdioh>
+#include<conio.h>
+void main()
+{
+int N,r;
+printf("enter the number");
+scanf("%d",&r);
+N=[1,2,3,4,5,6,7,8,9,10];
+if(N=r)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+

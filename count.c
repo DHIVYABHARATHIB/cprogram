979d11c67fb562aedf2e6b777af5fff3815b7315
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-10 18:53:49 +0530

    Create new file

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("the count is %d",count);
+}

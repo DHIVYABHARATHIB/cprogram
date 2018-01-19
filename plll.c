a581ccd99384110e26ae30491c202f3ab887b19e
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-19 15:50:02 +0530

    Rename rr.c to plll.c

---
+++ plll.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[100];
+printf("enter the string");
+int i,alphabets;
+while(s[i]=!0)
+{
+if((i<='a'&&i<='z')||(i<='A'&&i<='Z')
+{
+alphabets++;
+}
+}

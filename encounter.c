75cfe425ee9c6aa1c37da0dabd48d6c933c5e229
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-19 19:33:29 +0530

    Create encounter.c

---
+++ encounter.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m1,m2,n;
+printf("enter the two number");
+scanf("%d%d",&m1&m2);
+n=m1+m2;
+if(n%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+}

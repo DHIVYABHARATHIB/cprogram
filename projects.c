4a9b7f9feb5915564b2ba92cf73e6cc629e93bd5
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-19 16:37:53 +0530

    Create projects.c

---
+++ projects.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m1,m2,k;
+printf("enter the two number");
+scanf("%d%d",&m1&m2);
+k=m1*m2;
+if(k%2==0)
+{
+printf("even");
+}
+else
+{
+printf("not even");
+}
+}

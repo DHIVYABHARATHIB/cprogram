8b931c1b873b58b59714ec0ac3e9deefdbfdfc7b
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-19 16:48:08 +0530

    Create tiger.c

---
+++ tiger.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.H>
+void main();
+{
+int n1,n2,t;
+printf("enter the two number");
+printf("the values before swapping");
+swap(n1,n2);
+t=n1;
+n1=n2;
+n2=t;
+printf("the value after swapping");
+}

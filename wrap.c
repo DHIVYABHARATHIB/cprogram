22925c6f2c4a977e8a2820635a3e2a0a9eb7c518
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-24 16:37:16 +0530

    Create wrap.c

---
+++ wrap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+char s[10];
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the string ");
+scanf("%s",&s[10]);
+g=s[10].n;
+if(((g>='a')&&(g<='z'))&&((g>='A')&&(g<='Z'))&&((g>='0'))&&((g<='9')))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}

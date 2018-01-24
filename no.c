477b7b440ff951af38def0c877cef9eaa8320328
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-24 15:54:02 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n=9,m=6,h;
+printf("enter the number");
+scanf("%d",&h);
+if((h<=n)&&(h>=m))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}

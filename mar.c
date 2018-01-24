155feda5aa872fadfbcf3848fd2a4ea11f94419d
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-24 15:40:18 +0530

    Create mar.c

---
+++ mar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int count=0,i;
+char str[100];
+printf("enter the string");
+scanf("%s",&str[i]);
+for(i=0;str[i]=0;i++);
+{
+if(str[i]=' ')
+{
+++count;
+}
+printf("the count is %d",count+1);
+}
+}

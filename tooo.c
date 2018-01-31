18f88f41c5d4f3c084c40deea7bb560a0566facc
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-31 15:40:52 +0530

    Create tooo.c

---
+++ tooo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,d;
+printf("enter the numbers");
+scanf("%d%d",&n1,&n2);
+d=n1-n2;
+if((n%2)==0)
+{
+printf("even");
+}
+else
+{
+printf("no");
+}
+}

cf9ccae5c7d188d7688f41b8e02073053a713887
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-31 15:56:05 +0530

    Create pro1.c

---
+++ pro1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+printf("%d",&n);
+if(n%2==0)
+{
+printf("no is %d",n-2);
+}
+else
+{
+printf("no is %d",n-1);
+}
+}

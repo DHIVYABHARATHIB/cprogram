b1a5cb663e119a3d942e6b19ea93ab4663004b00
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-01-31 16:29:50 +0530

    Create pro4.c

---
+++ pro4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int count;
+char d[90];
+printf("enter the string");
+scanf("%d",&d);
+if(d==" ")
+{
+count++;
+printf("the count is %d",count);
+}
+}
+

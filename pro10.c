2db3d13d1c1c6af2a87978144954429ffd4f2fc2
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-02 15:45:34 +0530

    Rename pro9.c to pro10.c

---
+++ pro10.c
@@ -0,0 +1,16 @@
+#inlcude<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(((n%10)==0)&&((n%10)==1))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}

a7b6a998a47942752741124bf7ae4eee116f509b
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-02 16:40:28 +0530

    Create pro13.c

---
+++ pro13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char v[100],c[100];
+printf("enter the string");
+scanf("%s",&v);
+strcpy(c[100],v[100);
+strrev(c[100]);
+strcmp((c[100],v[100])==0)
+{
+printf("palindrome");
+}
+printf("not a palindrme");
+}

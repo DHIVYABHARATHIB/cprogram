c940666531fec565214b94200b541c76286888b7
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-05 16:14:19 +0530

    Create pro 17.c

---
+++ pro 17.c
@@ -0,0 +1,20 @@
+#include<stdiio.h>
+#include<conio.h>
+void main()
+{
+char n[100];
+char g[6]={'a','e','i','o','u'};
+printf("enter the string");
+scanf("%d",&n);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]=g[6])
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+}

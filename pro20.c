36013b1c1c21d300355fc29f42ed27d701efb635
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-07 16:35:40 +0530

    Create pro20.c

---
+++ pro20.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+char str[10];
+printf("enter the string");
+scanf("%s",str);
+l=strlen(str);
+if(l%2==0)
+{
+g=str(l/2,'*');
+printf("%d",*);
+}
+else
+{
+g=str((l-1)/2,'*');
+printf("%d",'*');
+}
+}

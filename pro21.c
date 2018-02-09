69fce17c5f72be19c34896adcc4b52a4c464126f
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-09 16:08:24 +0530

    Update pro21.c

--- pro21.c
+++ pro21.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-const float add = 0.5;
+const float n = 0.5;
 int main(void)
 {
-      float v;
+      float a;
 
        printf("Please enter a value to be rounded: ");
-       scanf("%f", &v);
+       scanf("%f", &n);
 
-       v = v + add;
-       v = (int)v;
+       n = n + add;
+       n = (int)n;
 
-       printf("The rounded value is %.2f", v);
+       printf("The rounded value is %.2f", n);
 
       return 0;
 }

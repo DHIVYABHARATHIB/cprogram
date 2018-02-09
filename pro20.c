7203a663124edb3b9a08a9b0ac72f31a26fe08bb
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-09 15:32:58 +0530

    Update pro20.c

--- pro20.c
+++ pro20.c
@@ -4,17 +4,20 @@
 void main()
 {
 char str[10];
+  int i,l;
 printf("enter the string");
 scanf("%s",str);
-l=strlen(str);
-if(l%2==0)
-{
-g=str(l/2,'*');
-printf("%d",*);
-}
-else
-{
-g=str((l-1)/2,'*');
-printf("%d",'*');
-}
+  l=strlen(str);
+  if(l%2==0)
+  {
+    str[l/2]='*';
+  }
+  else
+  {
+    str[(l-1)/2]='*';
+  }
+  for(i=0;str[i]!='\0';i++)
+  {
+    printf("the middle element is %c",str[i]);
+  }
 }

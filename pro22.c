842f5e884ccae05888ffe71f682dfce80e5b8d7c
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-09 16:48:09 +0530

    Create pro22.c

---
+++ pro22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,rev=0,r;
+printf("enter the string");
+scanf("%n);
+while(n!=0)
+{
+r=n%10;
+rev=(rev*10)+r);
+n=n/10;
+}
+printf("the reverse number is %d",rev);
+}

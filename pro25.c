ab8ebca309a4f683543cf8f3fb9ff0550d0daea0
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-12 15:36:39 +0530

    Create pro25.c

---
+++ pro25.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b,l,n,y,h;
+printf("enter the breadth ,length,height");
+scanf("%d%d%d",&b,&l,&,n);
+y=b*l*n;
+h=(2*b*l)+(2*l*n)+(2*n*b);
+printf("the volume of cubiod is %d",y);
+printf("the surface of the cubiod is %d",h);
+}

d333c5c3f29131aba3f2935c29c4600f7439e3bb
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-19 16:35:57 +0530

    Update pro29.c

--- pro29.c
+++ pro29.c
@@ -2,14 +2,17 @@
 #include<conio.h>
 void main()
 {
-int n[10];
-printf("the character array ")
+int n[10],i;
+printf("the character array ");
 for(i=0;i<10;i++)
 {
 scanf("%c",&n[i]);
 }
-if((n[i]>='0')&&(n[i]<='9')
+for(i=0;i<=10;i++)
+{
+if((n[i]>='0')&&(n[i]<='9'))
 {
 printf("the numbers are %d:",n[i]);
+}
 }
 }

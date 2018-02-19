172f9fd4a7ac15166ddfa8f3a6a934c7c7579807
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-19 15:35:44 +0530

    Create pro30.c

---
+++ pro30.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int p,n,r,d;
+printf("enter the principal amount,time,rate");
+scanf("%d %d %d",&p,&n,&r);
+d=(p*n*r)/100;
+printf("the pnr value is %d",d);
+}

aee57972b788d39fe3a668748aaf93eb469a448d
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-19 16:04:16 +0530

    Create pro33.c

---
+++ pro33.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5],i,k;
+printf("enter the array");
+printf(the k value");
+scanf("%d",&k);
+for(i=0;i<=5;i++)
+{
+printf("\n the array elemnts are %d",i);
+scanf("%d",&a[i]);
+}
+for(i=0;i<=5;i++)
+{
+if(a[i]==k)
+{
+printf("the k th value is %d",a[i]);
+}
+}
+}

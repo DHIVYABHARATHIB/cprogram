47af8c257e687915bb78fede1a25ae4b439fc1bf
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-02-26 15:43:21 +0530

    Create pro40.c

---
+++ pro40.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+3include<conio.h>
+void main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("A");
+break;
+case 2:
+printf("B");
+break;
+case 3:
+printf("C");
+break;
+case 4:
+printf("D");
+break;
+case 5:
+printf("E");
+break;
+case 6:
+printf("F");
+break;
+case 7:
+printf("G");
+break;
+case 8:
+printf("H");
+break;
+case 9:
+printf("I");
+break;
+case 10:
+printf("J");
+break;
+}

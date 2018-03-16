047b4eccecc84db2ba7ff77b97c13e816c3ee98f
DHIVYABHARATHIB <31499282+DHIVYABHARATHIB@users.noreply.github.com>
2018-03-16 16:35:28 +0530

    Create pro61.c

---
+++ pro61.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,k;
+for(i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",k);
+for(i=k;i<5;i++)
+{
+min=a[k];
+if(min>a[i])
+{
+min=a[i]);
+}
+}
+printf("\n the smallest number %d",min);
+}
